cfe75a71aa5b338808858b2161041014903dde66
leobeal <leonardobeal@gmail.com>
2018-05-23 23:24:59 +0200

    adds guarded in question and question option model to allow mass assignment

--- src/Model/Question.php
+++ src/Model/Question.php
@@ -3,11 +3,13 @@
 namespace Leobeal\LaravelQuiz;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model
 {
+    protected $guarded = ['id'];
+
     public function quiz(): BelongsTo
     {
         return $this->belongsTo(Quiz::class);
--- src/Model/QuestionOption.php
+++ src/Model/QuestionOption.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class QuestionOption extends Model
 {
+    protected $guarded = ['id'];
+
     public function question(): BelongsTo
     {
         return $this->belongsTo(Question::class);
